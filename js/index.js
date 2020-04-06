433b16a97d93438500aeec522cdc4551dd02e750
Azad1993 <azad.shahbazov@mail.ru>
2020-04-07 01:43:03 +0400

    Talant.az

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.carousel').carousel()
+});
+document.documentElement.setAttribute("lang", "en");
+document.documentElement.removeAttribute("class");
+
+axe.run(function(err, results) {
+    console.log(results.violations);
+});
